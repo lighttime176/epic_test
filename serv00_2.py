7dadb8ab2671f50b5506aab9102b56e8a596b426
lighttime176 <69344060+lighttime176@users.noreply.github.com>
2024-12-11 10:56:58 +0800

    Update serv00_2.py

--- serv00_2.py
+++ serv00_2.py
@@ -79,63 +79,63 @@ co.incognito()
 # 用该配置创建页面对象
 browser = Chromium(addr_or_opts=co)
 tab = browser.latest_tab
+for i_cishu in range(10):
+    tab.listen.start(targets='captcha')  # 开始监听，指定获取包含该文本的数据包
+    tab.get('https://www.serv00.com/offer/create_new_account')
+    res = tab.listen.wait().response
+    res = res.url
+    logger.info(res)
+    response = requests.get(res)
+    # 检查请求是否成功
+    if response.status_code == 200:
+        # 打开一个文件并将图片内容写入
+        with open("example.jpg", "wb") as file:
+            file.write(response.content)
+        logger.info("图片下载并保存成功!")
+    else:
+        logger.info("下载失败，状态码：", response.status_code)
+    firstname = random_email()
+    ele = tab.ele('css=#id_first_name')
+    ele.input(firstnames[i_cishu])
 
-  tab.listen.start(targets='captcha')  # 开始监听，指定获取包含该文本的数据包
-  tab.get('https://www.serv00.com/offer/create_new_account')
-  res = tab.listen.wait().response
-  res = res.url
-  logger.info(res)
-  response = requests.get(res)
-  # 检查请求是否成功
-  if response.status_code == 200:
-      # 打开一个文件并将图片内容写入
-      with open("example.jpg", "wb") as file:
-          file.write(response.content)
-      logger.info("图片下载并保存成功!")
-  else:
-      logger.info("下载失败，状态码：", response.status_code)
-  firstname = random_email()
-  ele = tab.ele('css=#id_first_name')
-  ele.input(firstnames[i_cishu])
+    firstname = random_email()
+    ele = tab.ele('css=#id_last_name')
+    ele.input(lastnames[i_cishu])
 
-  firstname = random_email()
-  ele = tab.ele('css=#id_last_name')
-  ele.input(lastnames[i_cishu])
+    firstname = random_email()
+    ele = tab.ele('css=#id_username')
+    username = lastnames[i_cishu] + firstname
+    ele.input(username)
 
-  firstname = random_email()
-  ele = tab.ele('css=#id_username')
-  username = lastnames[i_cishu] + firstname
-  ele.input(username)
+    ele = tab.ele('css=#id_email')
+    ele.input('oyoovz@gmail.com')
 
-  ele = tab.ele('css=#id_email')
-  ele.input('oyoovz@gmail.com')
+    ocr = ddddocr.DdddOcr()
+    ocr.set_ranges(2)
+    image = open("example.jpg", "rb").read()
+    result = ocr.classification(image, probability=True)
+    s = ""
+    for i in result['probability']:
+        s += result['charsets'][i.index(max(i))]
 
-  ocr = ddddocr.DdddOcr()
-  ocr.set_ranges(2)
-  image = open("example.jpg", "rb").read()
-  result = ocr.classification(image, probability=True)
-  s = ""
-  for i in result['probability']:
-      s += result['charsets'][i.index(max(i))]
+    logger.info(s)
 
-  logger.info(s)
-
-  ele = tab.ele('css=#id_captcha_1')
-  #cap = input('输入验证码')
-  ele.input(s)
-  ele = tab.ele('css=#id_question2')
-  ele.input('free')
-  ele = tab.ele('css=#id_tos')
-  ele.click()
-  ele = tab.ele('css=body > section > div > div.columns > div.column.is-half > form > p.control.submit > button')
-  ele.click()
-  ele = tab.ele('css=body > section > div > div.columns > div.column.is-half > form > p.control.has-error > span',timeout=30)
-  logger.info(ele.text)
-  if(ele.text == 'Maintenance time. Try again later.'):
-      pass
-  elif(ele.text == 'Invalid CAPTCHA'):
-      pass
-  else:
-      anpush_payload['content'] = ele.text
-      #response = requests.post(anpush_url, headers=anpush_headers, data=anpush_payload)
-      exit()
+    ele = tab.ele('css=#id_captcha_1')
+    #cap = input('输入验证码')
+    ele.input(s)
+    ele = tab.ele('css=#id_question2')
+    ele.input('free')
+    ele = tab.ele('css=#id_tos')
+    ele.click()
+    ele = tab.ele('css=body > section > div > div.columns > div.column.is-half > form > p.control.submit > button')
+    ele.click()
+    ele = tab.ele('css=body > section > div > div.columns > div.column.is-half > form > p.control.has-error > span',timeout=30)
+    logger.info(ele.text)
+    if(ele.text == 'Maintenance time. Try again later.'):
+        pass
+    elif(ele.text == 'Invalid CAPTCHA'):
+        pass
+    else:
+        anpush_payload['content'] = ele.text
+        #response = requests.post(anpush_url, headers=anpush_headers, data=anpush_payload)
+        exit()
