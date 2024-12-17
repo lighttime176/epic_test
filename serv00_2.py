5258d33b3cb8e0389f4dfc0adc44bebfb390f12c
lighttime176 <69344060+lighttime176@users.noreply.github.com>
2024-12-17 13:19:28 +0800

    Update serv00_2.py

--- serv00_2.py
+++ serv00_2.py
@@ -4,6 +4,7 @@ import requests,logging
 import random
 import time
 import ddddocr
+import requests
 anpush_url = "https://api.anpush.com/push/125UR66POG8AAOPZLNIV0AK8426OHI"
 anpush_payload = {
     "title": "your_title",
@@ -13,6 +14,23 @@ anpush_payload = {
 anpush_headers = {
     "Content-Type": "application/x-www-form-urlencoded"
 }
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
+    'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+}
+response = requests.get('https://myip.ipip.net/', headers=headers)
+logger.info(response.text)
 firstnames = [
     "Liam", "Noah", "Oliver", "Elijah", "James", "William", "Benjamin", "Lucas", "Henry", "Alexander",
     "Mason", "Michael", "Ethan", "Daniel", "Jacob", "Jackson", "Logan", "David", "Joseph", "Samuel",
@@ -94,11 +112,11 @@ for i_cishu in range(10):
         logger.info("图片下载并保存成功!")
     else:
         logger.info("下载失败，状态码：", response.status_code)
-    firstname = random_email()
+    #firstname = random_email()
     ele = tab.ele('css=#id_first_name')
     ele.input(firstnames[i_cishu])
 
-    firstname = random_email()
+    #firstname = random_email()
     ele = tab.ele('css=#id_last_name')
     ele.input(lastnames[i_cishu])
 
